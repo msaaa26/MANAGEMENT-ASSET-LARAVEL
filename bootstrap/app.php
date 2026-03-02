5122150e38b29554f26034399fb8c0fe093275ef
Muhammad Isa <148967091+msaaa26@users.noreply.github.com>
2026-03-02 10:33:15 +0700

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        // ✅ TAMBAHKAN INI
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => App\Http\Middleware\RoleMiddleware::class,
         ]);
