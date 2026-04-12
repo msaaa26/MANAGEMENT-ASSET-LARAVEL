5a6bb68e2a06b4c6520b0df98bf560d0d51502f1
Muhammad Isa <148967091+msaaa26@users.noreply.github.com>
2026-04-12 18:57:30 +0700

    Change background image in login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -111,7 +111,7 @@
         </div>
         <div
           class="col-lg-8 col-12 order-lg-2 order-1 min-vh-100 background-walk-y position-relative overlay-gradient-bottom"
-          data-background="{{ asset('assets/img/unsplash/anime-style-scene-depicting-girls-friendship.jpg') }}">
+          data-background="{{ asset('assets/img/unsplash/vertical-shot-thick-forest-river.jpg') }}">
           <div class="absolute-bottom-left index-2">
             <div class="text-light p-5 pb-2">
               <div class="mb-5 pb-3">
